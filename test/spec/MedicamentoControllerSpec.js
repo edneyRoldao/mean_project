b042d9928a7a3d913c1c523bb135dad4e81d78b3
edneyRoldao <edneyroldao@gmail.com>
2016-11-24 00:47:10 -0200

    add more code

--- test/spec/MedicamentoControllerSpec.js
+++ test/spec/MedicamentoControllerSpec.js
@@ -1,17 +1,29 @@
 describe("MedicamentoController", function() {
 
 	var $scope;
+	var $httpBackend;
 
 	beforeEach(function() {
 		module("myPharmaco");
-		inject(function($injector) {
+		inject(function($injector, _$httpBackend_) {
 			$scope = $injector.get("$rootScope").$new();
+			$httpBackend = _$httpBackend_;
+			$httpBackend.when("GET", "/medicamento/1").respond({_id: "1"});
 		});
 	});
 
-	it("Must create a Medicamento empty when no one route parameter were passed", inject(function($controller) {
+	it("Must create a Medicamento object empty when no one route parameter were passed", inject(function($controller) {
 		$controller("MedicamentoController", {"$scope": $scope});
 		expect($scope.medicamento._id).toBeUndefined();
 	}));
+
+	it("Should fill Medicamento object when parameters are passed", inject(function($controller) {
+		$controller("MedicamentoController", {
+			"$routeParams": {id: 1},
+			"$scope": $scope
+		});
+
+		expect($scope.medicamento._id).toBeDefined();
+	}));	
 
 });
